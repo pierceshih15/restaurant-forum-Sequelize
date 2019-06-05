1a4e77e746db8fa00e98a5502662353b759d5787
pierce <pierceshih@gmail.com>
2019-06-05 18:40:02 +0800

    Install express-session connect-flash and Add confirm user password and unique email.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const handlebars = require('express-handlebars') // 引入 handlebars
 const bodyParser = require('body-parser');
+const flash = require('connect-flash')
+const session = require('express-session')
 const db = require('./models');
+
 const app = express()
 const port = 3000
 
@@ -12,6 +15,21 @@ app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引�
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+// setup session and flash
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(flash())
+
+// 把 req.flash 放到 res.locals 裡面
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   db.sequelize.sync() // 跟資料庫同步
--- controllers/userController.js
+++ controllers/userController.js
@@ -7,15 +7,33 @@ const userController = {
     return res.render('signup')
   },
   signUp: (req, res) => {
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-      })
-      .then(user => {
-        return res.redirect('/signin')
-      })
-  },
+    if (req.body.passwordCheck !== req.body.password) {
+      req.flash('error_messages', '兩次密碼輸入不一致！')
+      return res.redirect('/signup')
+    } else {
+      User.findOne({
+          where: {
+            email: req.body.email
+          }
+        })
+        .then(user => {
+          if (user) {
+            req.flash('error_messages', '信箱已被註冊使用！')
+            return res.redirect('/signup')
+          } else {
+            User.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+              })
+              .then(user => {
+                req.flash('success_messages', '帳號已成功註冊！')
+                return res.redirect('/signin')
+              })
+          }
+        })
+    }
+  }
 }
 
 module.exports = userController;
