fa6b3cbaf22bc754f12141609c670f565c4a2a3f
pierce <pierceshih@gmail.com>
2019-06-06 11:55:37 +0800

    Add isAdmin to users table and Add authentication before rendering pages.

--- models/user.js
+++ models/user.js
@@ -3,9 +3,10 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
-    password: DataTypes.STRING
+    password: DataTypes.STRING,
+    isAdmin: DataTypes.BOOLEAN
   }, {});
-  User.associate = function(models) {
+  User.associate = function (models) {
     // associations can be defined here
   };
   return User;
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,28 @@ const adminController = require('../controllers/adminController');
 const userController = require('../controllers/userController');
 
 module.exports = (app, passport) => {
-  app.get('/', (req, res) => res.redirect('/restaurants'));
-  app.get('/restaurants', restController.getRestaurants);
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect('/signin');
+  }
 
-  app.get('/admin', (req, res) => res.redirect('/admin/restaurants'));
-  app.get('/admin/restaurants', adminController.getRestaurants);
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) {
+        return next();
+      }
+      return res.redirect('signin');
+    }
+    res.redirect('/signin');
+  }
+
+  app.get('/', authenticated, (req, res) => res.redirect('/restaurants'));
+  app.get('/restaurants', authenticated, restController.getRestaurants);
+
+  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'));
+  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants);
 
   app.get('/signup', userController.signUpPage);
   app.post('/signup', userController.signUp);
