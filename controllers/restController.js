6e4f5df20bf5c3fea5d4c816370f4403ed9374ea
pierce <pierceshih@gmail.com>
2019-06-11 15:46:08 +0800

    Add restaurants index page.

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,7 +1,25 @@
+const db = require('../models');
+const Restaurant = db.Restaurant;
+const category = db.Category;
+
 // 宣告 restController 物件變數，管理不同物件屬性（函式）
 const restController = {
   getRestaurants: (req, res) => {
-    return res.render('restaurants');
+    Restaurant.findAll({
+        include: [category]
+      })
+      .then(restaurant => {
+        // 複製一份餐廳資料，存數變數 data 使用
+        const data = restaurant.map(r => ({
+          // 展開餐廳資料
+          ...r.dataValues,
+          // 複寫 description 內容
+          description: r.dataValues.description.substring(0, 50)
+        }))
+        return res.render('restaurants', {
+          restaurants: data
+        });
+      })
   }
 }
 
