e7741b7321f5b70c9041712df1a0faace076a471
pierce <pierceshih@gmail.com>
2019-06-11 11:03:51 +0800

    Add category seed data.

--- seeders/20190607114854-my-seed-file.js
+++ seeders/20190607114854-my-seed-file.js
@@ -26,6 +26,18 @@ module.exports = {
       createdAt: new Date(),
       updatedAt: new Date()
     }], {});
+
+    queryInterface.bulkInsert('Categories',
+      ['中式料理', '日本料理', '義大利料理', '墨西哥料理', '素食料理', '美式料理', '複合式料理']
+      .map((item, index) =>
+        ({
+          id: index + 1,
+          name: item,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        })
+      ), {});
+
     return queryInterface.bulkInsert('Restaurants',
       Array.from({
         length: 50
@@ -38,13 +50,15 @@ module.exports = {
           image: faker.image.imageUrl(),
           description: faker.lorem.text(),
           createdAt: new Date(),
-          updatedAt: new Date()
+          updatedAt: new Date(),
+          CategoryId: Math.floor(Math.random() * 5) + 1
         })
       ), {});
   },
 
   down: (queryInterface, Sequelize) => {
     queryInterface.bulkDelete('Users', null, {})
+    queryInterface.bulkDelete('Categories', null, {})
     return queryInterface.bulkDelete('Restaurants', null, {});
   }
 };
